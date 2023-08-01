6a52ab89a3136f04c658bd82b193f7d2c20a3156
Bernardo Nacif <89922059+bernardonacif@users.noreply.github.com>
2023-08-01 14:40:43 -0300

    add android files

---
+++ AplicPisoANDROID/src/com/example/aplicpisoandroid/AplicPiso2017Activity.java
@@ -0,0 +1,25 @@
+package com.example.aplicpisoandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AplicPiso2017Activity extends Activity {
+		
+	EditText editText1;
+	EditText editText2;
+	EditText editText3;
+	EditText editText4;
+	EditText editText5;
+	EditText editText6;
+	Button button1;
+	
+	
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplic_piso2017);
+    }
+}
